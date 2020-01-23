7d00444757f95dfea887dbfc1cbf100ec7c65b5e
Óscar V.H <oscarvegaherrera59@gmail.com>
2020-01-23 21:44:43 +0100

    Añadiendo tablas nuevas

---
+++ database/migrations/2020_01_23_203551_create_perfiles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('image')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('comment')->nullable();
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfiles');
+    }
+}
